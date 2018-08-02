9c8abf941b818ed7f4a7eb3da8f15e20a26693ac
David Ackerman <david.s.ackerman@gmail.com>
2018-08-01 18:44:15 -0600

    separate out some helper code and start getting into scraping based on classes

---
+++ Technology/WebScraping/notebook_code/scraping_support.py
@@ -0,0 +1,17 @@
+from html.parser import HTMLParser
+
+class TagRemover(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def remove_tags(html):
+    s = TagRemover()
+    s.feed(html)
+    return s.get_data()
