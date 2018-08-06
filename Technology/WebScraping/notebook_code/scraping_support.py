87b258abf0b238f29e7d743029a081b5817fcc51
David Ackerman <david.s.ackerman@gmail.com>
2018-08-06 00:46:11 -0600

    Last additions to the real website scraping example, including charts. More narrative explaining process.

--- Technology/WebScraping/notebook_code/scraping_support.py
+++ Technology/WebScraping/notebook_code/scraping_support.py
@@ -1,4 +1,9 @@
 from html.parser import HTMLParser
+from textblob import TextBlob
+import nltk
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
 class TagRemover(HTMLParser):
     def __init__(self):
@@ -15,3 +20,21 @@ def remove_tags(html):
     s = TagRemover()
     s.feed(html)
     return s.get_data()
+
+def interesting_words(text):
+    blob = TextBlob(text)
+    blob.words
+
+    all_words = blob.words.lower()
+    remove_words = stopwords.words('english') + ["'s", "n't", "'re", "'ll"]
+    interesting_words = set(all_words) - set(remove_words)
+
+    selected_words = []
+    for word in all_words:
+        if word in interesting_words:
+            selected_words.append(word)
+            
+    return selected_words
+
+def reverse_column(forward_column):
+    return list(reversed(list(forward_column)))
