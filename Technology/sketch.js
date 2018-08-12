0752f06d2b59b81d51b665a7b12045c5a4e6ede5
jchalatu <jchalatu@ualberta.ca>
2018-08-12 23:08:05 +0000

    Added slideshow in a cell

--- Technology/sketch.js
+++ Technology/sketch.js
@@ -1,23 +1,26 @@
-// var img = [];
-// var paths = ["coal.jpg","sun.jpg","solar.jpg"];
+var img = [];
+var paths = ["images/coal.jpg","images/nat-gas.jpg","images/oil.jpeg","images/biofuel.jpg","images/geothermal.jpg","images/nuclear.jpg","images/solar.jpg"];
 
 // textFont("Helvetica");
 // text("Attention, please.", 50, 200);
-var bcolor = [0,50,100,150,200,250];
 var button;
 var i;
 function setup() {
-    createCanvas(700, 400);
+    createCanvas(650, 500);
     button = select('button');
     button.mousePressed(moveIndx);
+    for (var j = 0; j < paths.length; j++) {
+        img[j] = loadImage(paths[j]);
+    }
     i = 0;
 }
 function moveIndx() {
     i += 1;
-    if (i > bcolor.length - 1) {
+    if (i > paths.length - 1) {
         i = 0
     }
 }
 function draw() {
-    background(bcolor[i]);
+    background(0);
+    image(img[i],0,0);
 }
