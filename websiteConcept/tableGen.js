03fe7bc012245f05ac14d931ed37465d1286b906
Vincent <vincent.cote@uleth.ca>
2019-04-29 13:19:42 -0600

    Generates a list in html from data collected within notebooks

---
+++ websiteConcept/tableGen.js
@@ -0,0 +1,9 @@
+
+$.getJSON("data.json", function(json) {
+    for (notebook in json) {
+        let data = json[notebook];
+        let info = `<li><h2>${data.Title}</h2> <p><b>Grade:</b> ${data.Grade} <b>Subject:</b> ${data.Subject}</p> <p>${data.Description}</p><a href="${data.Hyperlink}">Click here for Notebook</a></li>`
+        $('#NotebookList').append(info);
+    }
+
+})
